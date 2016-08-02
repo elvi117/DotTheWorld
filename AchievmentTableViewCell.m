0e0f24e2f90943de7778b100a329f7884ad594d4
Lukasz Matuszczak <lukaszmatuszczak@Wojciechs-MacBook-Pro.local>
2016-08-02 10:29:21 +0200

    fix syntax part3

--- AchievmentTableViewCell.m
+++ AchievmentTableViewCell.m
@@ -9,14 +9,4 @@
 #import "AchievmentTableViewCell.h"
 @implementation AchievmentTableViewCell
 
-- (void)awakeFromNib {
-    [super awakeFromNib];
-    // Initialization code
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
-}
-
 @end
